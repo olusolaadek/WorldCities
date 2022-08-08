341fcddae11effad8468717e368310f5ca11216a
OLUSOLA ADEKUNLE <olusola.adekunle@outlook.com>
2022-08-08 08:16:51 +0100

    Implement pagination on cities  using MatPaginator

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource, MatTable } from "@angular/material/table";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
 import { environment } from 'src/environments/environment.prod';
+
 import { City } from './city';
 
 @Component({
@@ -10,16 +12,20 @@ import { City } from './city';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
-
-  public cities!: City[];
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+  // public cities!: City[];
+  public cities!: MatTableDataSource<City>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<City[]>(environment.baseUrl + 'api/cities').subscribe(
       result => {
-        this.cities = result;
+        // this.cities = result;
+        this.cities = new MatTableDataSource<City>(result);
+        this.cities.paginator = this.paginator;
 
       }, error => {
         console.log(error);
