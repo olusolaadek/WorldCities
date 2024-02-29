2e481775be8ffc800b5bf86c82fcd7c08da60d32
Olusola <olusolaadek@hotmail.com>
2024-02-29 12:10:45 -0700

    configure apollo client

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,22 +1,35 @@
-import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
+import { NgModule } from '@angular/core';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { environment } from 'src/environments/environment';
 
 const uri = 'https://localhost:40443/api/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-  return {
-    link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
-  };
-}
+// export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+//   return {
+//     link: httpLink.create({ uri }),
+//     cache: new InMemoryCache(),
+//   };
+// }
 
 @NgModule({
   exports: [ApolloModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache({
+            addTypename: false,
+          }),
+          link: httpLink.create({
+            uri: environment.baseUrl + 'api/graphql',
+          }),
+          defaultOptions: {
+            watchQuery: { fetchPolicy: 'no-cache' },
+          },
+        };
+      },
       deps: [HttpLink],
     },
   ],
